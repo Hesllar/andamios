ec77b92a261b63daf94a1a86c29a8f0d945630aa
Hesllar Henry <hesllar.linzmayer@gmail.com>
2025-07-15 13:17:44 -0400

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Andamios - J&V",
   description: "Arriendos de andamios y servicios de construcción en Chile",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
