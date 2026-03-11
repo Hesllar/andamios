ca604a75bf35d256b26ed443a05871c79740bdf3
Hesllar Henry <hesllar.linzmayer@gmail.com>
2026-03-10 23:33:42 -0300

    correción de responsividad en pantallas xl

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -10,7 +10,7 @@ export function Container(props: Readonly<ContainerProps>) {
   return (
     <div
       ref={props.refGeneric}
-      className={`container px-6 py-4 sm:p-8 mx-auto xl:px-0 ${
+      className={`container px-6 py-4 sm:p-8 mx-auto ${
         props.className ? props.className : ""
       }`}
     >
